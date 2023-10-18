9e7a75b6f4b0de3ef23cd8f0c4b72ccf27b13d76
Diego Araujo <diegolaraujo96@gmail.com>
2023-10-18 11:53:55 -0300

    Nos persisting state bug.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,56 +1,87 @@
 import Head from "next/head";
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { HTMLAttributes, useCallback, useEffect, useMemo, useState } from "react";
+
+type Line = [number, number, number, number]
 
 const rows = [2,3,4,5,6,7]
 const columns = [2,3,4,5,6,7]
 
+const generateLinesForBox = (row: number, col: number): Line[] => {
+  return [
+    // [(a, b), (c, d)]
+    [col - 1, row - 1, col - 1, row],
+    [col - 1, row - 1, col, row - 1],
+    [col - 1, row, col, row],
+    [col, row - 1, col, row]
+  ]
+}
+
+const lineIsVertical = (line: Line) => line[0] === line[2]
+
+const renderLine = (line: Line): JSX.Element => {
+  const [a, b, c, d] = line
+  if (lineIsVertical(line)) {
+    return <div key={`${a}_${b}_${c}_${d}`} style={{
+      gridColumnStart: a + 1,
+      gridRowStart: Math.max(b, d),
+      width: "2px",
+      background: "black"
+    }}></div>
+  } else {
+    return <div key={`${a}_${b}_${c}_${d}`} style={{
+      gridColumnStart: Math.max(a, c),
+      gridRowStart: b + 1,
+      height: "2px",
+      background: "black"
+    }}></div>
+  }
+}
+
 export default function Home() {
-  const [selected, setSelected] = useState<Record<string, boolean>>({})
-  const resetSelected = useCallback(() => setSelected({}), [])
+  const [selected, setSelected] = useState<Line[]>([])
 
   const [isMouseDown, setIsMouseDown] = useState(false);
-  const [deleteMode, setDeleteMode] = useState(false)
+  const [deleteMode, setDeleteMode] = useState(false);
 
-  useEffect(() => {
-    const handleMouseUp = () => {
-      setIsMouseDown(false)
-    }
-
-    window.addEventListener('mouseup', handleMouseUp)
-
-    return () => {
-      window.removeEventListener('mouseup', handleMouseUp)
-    }
-  }, [])
+  console.log("selected", selected)
 
   const handleMouseDown = (row: number, col: number) => {
-    if (selected[`${row}_${col}`]) {
-      setDeleteMode(true)
-    } else {
-      setDeleteMode(false)
-    }
-    setSelected({...selected, [`${row}_${col}`]: !selected[`${row}_${col}`]})
-    setIsMouseDown(true);
+    console.log("selected ->", selected)
+    setSelected(generateLinesForBox(row, col))
+    // setIsMouseDown(true);
   };
+
+  const resetSelected = () => setSelected([])
 
   const handleMouseEnter = (row: number, col: number) => {
+    console.log("mouse enter", row, col)
     if (isMouseDown) {
-      setSelected({...selected, [`${row}_${col}`]: deleteMode ? false : true})
     }
   };
 
-  const canvasBoxes = useMemo(() => rows.map((row) => {
-    return columns.map((col) => {
-      return <div key={`${row}_${col}`} style={{
-        gridRowStart: row,
-        gridColumnStart: col,
-        border: `${selected[`${row}_${col}`] ? "4px solid black" : "0.25px solid black"}`
-      }} 
-        onMouseDown={() => handleMouseDown(row, col)}
-        onMouseEnter={() => handleMouseEnter(row, col)}
-      ></div>
-    })
-  }).flat(), [selected, isMouseDown])
+  const canvasBoxes = useMemo(() => rows.flatMap((row) => columns.map((col) => <div key={`${row}_${col}`}
+    style={{
+      gridRowStart: row,
+      gridColumnStart: col,
+      border: "0.5px dashed black"
+    }} 
+    onMouseDown={() => handleMouseDown(row, col)}
+    // onMouseEnter={() => handleMouseEnter(row, col)}
+  ></div>)), [])
+
+  const lines = selected.map(renderLine)
+
+  // useEffect(() => {
+  //   const handleMouseUp = () => {
+  //     setIsMouseDown(false)
+  //   }
+
+  //   window.addEventListener('mouseup', handleMouseUp)
+
+  //   return () => {
+  //     window.removeEventListener('mouseup', handleMouseUp)
+  //   }
+  // }, [])
 
   return (
     <>
@@ -61,13 +92,11 @@ export default function Home() {
       </Head>
       <main className="min-h-screen">
         <div className={`min-h-screen modules-canvas`}>
-
-          <div className="row-start-1 col-start-7 flex items-center justify-center">
+          <div className="row-start-1 col-start-8 flex items-center justify-center">
             <button className="btn btn-primary" onClick={resetSelected}>Reset</button>
           </div>
-
           {canvasBoxes}
-
+          {lines}
         </div>
       </main>
     </>
